0482f84a995983b0e328cc84a6ed24755143216a
arnavzed147-ship-it <arnavzed147@gmail.com>
2025-08-15 03:04:28 +0700

    Initial commit: Artan Protec site

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-red-600">Artan Protec</h1>
+      <p className="mt-4 text-gray-700">Advanced Protection. Engineered Performance.</p>
+    </div>
+  )
+}
